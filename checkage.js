646ba9494043fe7b0ada7eb0a7714044ea9a34df
route-2 <ruthurao@gmail.com>
2022-07-31 13:48:31 +0530

    img

--- checkage.js
+++ checkage.js
@@ -2,6 +2,7 @@ let people = [
     { name: "John", age: 20 },
     { name: "Jane", age: 21 },
     { name: "Joe", age: 22 },
+    { name: "Ron", age: 20 },
 ];
 
 function groupBy(ar)
@@ -9,10 +10,10 @@ function groupBy(ar)
     var check = {}; // empty object to check if the key already exists in the object
     ar.forEach((item) => {
         if(item.age in check) {
-            check[item.age].push(item.name);
+            check[item.age].push(item.name + " " + item.age);
         }
         else {
-            check[item.age] = [item];
+            check[item.age] = [item.name + " " + item.age];
         }
     })
     return check
